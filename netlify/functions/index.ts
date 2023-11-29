b053378914b60fbde24cb0f6c807f7615450799b
gvdp <vdputteglenn@gmail.com>
2023-11-29 22:10:32 +0100

    chore: use functions as published npm dependency

--- netlify/functions/index.ts
+++ netlify/functions/index.ts
@@ -1,29 +1,3 @@
-import { Handler } from "@netlify/functions";
-import axios, { AxiosError } from "axios";
-import { fetchTokens } from "../../src/google-tokens";
+import { homeHandler } from "sheets-as-an-api-functions";
 
-export const handler: Handler = async (event) => {
-  const { code } = event.queryStringParameters as { code: string };
-
-  if (!code) {
-    return { statusCode: 404 };
-  }
-
-  try {
-    const tokens = await fetchTokens(code, "http://localhost:8888");
-
-    return {
-      statusCode: 200,
-      body: `
-        ntl env:set REFRESH_TOKEN ${tokens.refresh_token}
-      `,
-    };
-  } catch (e) {
-    if (axios.isAxiosError(e)) {
-      console.error((e as AxiosError).response?.data);
-    } else {
-      console.error(e);
-    }
-    return { statusCode: 500 };
-  }
-};
+export const handler = homeHandler;
--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,6 @@
-export const SHEET_IDS: Record<string, { name: string; sheetID: string }> = {
+import { SheetConfig } from "sheets-as-an-api-functions";
+
+export const SHEET_IDS: SheetConfig= {
   STUFF: {
     sheetID: '1Xr80BAWG3oI6l0jjA8mEzbDzUNupgsPXwDOM0vqUgDs',
     name: 'Stuff',
